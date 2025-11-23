069ddf9cdb27e79fe880a8a48f22cf950336866f
Stuart Baumann <stuart@stuartbaumann.com>
2025-11-23 06:15:13 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# PivotTable
+
+This is a wrapper over PivotTableJS. You can use it to embed your data into a html page. Once in there you can visualise the data like a pivottable. You can see examples here https://pivottable.js.org/examples/index.html.
+
+The functionality is quite similar to the python module. One change is that it is possible to put multiple different tables onto the same page (either sharing or not sharing data sources). It is also easy to change the colour mapping for use in HeatMap.
+
+As an example see the following. This produces the file pivottable.html
+
+```
+
+using PivotTables, DataFrames, Dates
+
+stockReturns = DataFrame(
+    Symbol = ["RTX", "RTX", "RTX", "GOOG", "GOOG", "GOOG", "MSFT", "MSFT", "MSFT"],
+    Date = Date.(["2023-01-01", "2023-01-02", "2023-01-03", "2023-01-01", "2023-01-02", "2023-01-03", "2023-01-01", "2023-01-02", "2023-01-03"]),
+    Return = [0.01, -0.005, 0.002, 0.015, 0.01, -0.003, 0.008, 0.004, -0.002]
+)   
+
+correlations = DataFrame(
+    Symbol1 = ["RTX", "RTX", "GOOG", "RTX", "GOOG", "MSFT", "GOOG", "MSFT", "MSFT",],
+    Symbol2 = ["GOOG", "MSFT", "MSFT", "RTX", "GOOG", "MSFT", "RTX", "RTX", "GOOG",],
+    Correlation = [-0.85, -0.75, 0.80, 1.0, 1.0, 1.0, -0.85, -0.75, 0.80]
+)
+
+exclusions = Dict(
+    :Symbol => [:MSFT]
+)
+
+
+pt = PivotTable(:Returns_Over_Last_Few_Days, :stockReturns;
+    rows = [:Symbol],
+    cols = [:Date],
+    vals = :Return,
+    exclusions = exclusions,
+    colour_map = Dict{Float64,String}([-0.05, 0.0, 0.05] .=> ["#9e0303", "#ffffff", "#00e32d"]),
+    aggregatorName = :Average,
+    rendererName = :Heatmap
+)
+
+pt2 = PivotTable(:Correlation_Matrix, :correlations;
+    rows = [:Symbol1],
+    cols = [:Symbol2],
+    vals = :Correlation,
+    colour_map = Dict{Float64,String}([-1.0, 0.0, 1.0] .=> ["#FF4545", "#ffffff", "#4F92FF"]),
+    aggregatorName = :Average,
+    rendererName = :Heatmap
+)
+
+pge = PivotTablePage(Dict{Symbol,DataFrame}(:stockReturns => stockReturns, :correlations => correlations), [pt, pt2])
+create_pivot_table_html(pge,"pivottable.html")
+
+```
+
+
---
+++ src/PivotTables.jl
@@ -0,0 +1,160 @@
+module PivotTables
+
+
+    using CSV, DataFrames, JSON
+
+    const pivottable_function_template = raw"""
+        $("#__NAME_OF_PLOT___").pivotUI(
+                            $.csv.toArrays($("#__NAME_OF_DATA___").text()),
+                            $.extend({
+                                renderers: $.extend(
+                                    $.pivotUtilities.renderers,
+                                    $.pivotUtilities.c3_renderers,
+                                    $.pivotUtilities.d3_renderers,
+                                    $.pivotUtilities.export_renderers
+                                    ),
+                                hiddenAttributes: [""]
+                            }, ___KEYARGS_LOCATION___)
+                        );
+    """
+
+
+
+
+
+    struct PivotTable
+        chart_title::Symbol
+        data_label::Symbol
+        pivottable_html::String
+        function PivotTable(chart_title::Symbol, data_label::Symbol;
+                                rows::Union{Missing,Vector{Symbol}} = missing, cols::Union{Missing,Vector{Symbol}} = missing, vals::Union{Missing,Symbol} = missing,
+                                inclusions::Union{Missing,Dict{Symbol,Vector{Symbol}}}= missing,
+                                exclusions::Union{Missing,Dict{Symbol,Vector{Symbol}}}=missing,
+                                colour_map::Union{Missing,Dict{Float64,String}}= Dict{Float64,String}([-2.0, -1.0, 0.0, 1.0, 2.0] .=> ["#FF4545", "#FFE045" , "#ffffff", "#42f55a", "#4F92FF"]),
+                                aggregatorName::Symbol=:Average,
+                                rendererName::Symbol=:Heatmap,
+                                rendererOptions::Union{Missing,Dict{Symbol,Any}}=missing)
+            
+            kwargs_d = Dict{Symbol,Any}()
+            if ismissing(rows) == false kwargs_d[:rows] = rows end
+            if ismissing(cols) == false kwargs_d[:cols] = cols end
+            if ismissing(vals) == false kwargs_d[:vals] = [vals] end
+            if ismissing(inclusions) == false kwargs_d[:inclusions] = inclusions end
+            if ismissing(exclusions) == false kwargs_d[:exclusions] = exclusions end
+            if ismissing(aggregatorName) == false kwargs_d[:aggregatorName] = aggregatorName end
+            if ismissing(rendererName) == false kwargs_d[:rendererName] = rendererName end
+            if ismissing(rendererOptions) == false
+                kwargs_d[:rendererOptions] = rendererOptions
+            end
+            if ismissing(rendererOptions) && (ismissing(colour_map) == false)
+                kwargs_d[:rendererOptions] = "___rendererOptions___"
+            end
+            kwargs_json = JSON.json(kwargs_d)
+
+            strr = replace(pivottable_function_template, "__NAME_OF_PLOT___" => replace(string(chart_title), " " => "_"), "__NAME_OF_DATA___" => replace(string(data_label), " " => "_"))
+            strr = replace(strr, "___KEYARGS_LOCATION___" => kwargs_json)
+            if ismissing(colour_map) == false
+                colour_values = sort(collect(keys(colour_map)))
+                colours = [colour_map[x] for x in colour_values]
+                strr = replace(strr, "\"___rendererOptions___\"" => "{ heatmap: { colorScaleGenerator: function(values) { return Plotly.d3.scale.linear().domain(" * string(colour_values) * ").range(" * string(colours) * ")}}}")
+            end
+
+            new(chart_title, data_label, strr)
+        end
+    end
+
+
+    struct PivotTablePage
+        dataframes::Dict{Symbol,DataFrame}
+        pivot_tables::Vector{PivotTable}
+    end
+
+    const DATASET_TEMPLATE = raw"""<div id="___DDATA_LABEL___" style="display: none;">___DATA1___</div>"""
+
+    const PIVOTTABLE_IN_PAGE_TEMPLATE = raw"""
+        <h2>___TABLE_HEADING___</h2>
+        <div id="__FUNCTION_NAME___"></div>
+
+        <br><hr><br>
+    """
+
+    const FULL_PAGE_TEMPLATE = raw"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PivotTable.js</title>
+
+        <!-- external libs -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.11/c3.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/0.71/jquery.csv-0.71.min.js"></script>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.19.0/pivot.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.19.0/pivot.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.19.0/d3_renderers.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.19.0/c3_renderers.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.19.0/export_renderers.min.js"></script>
+    </head>
+
+    <body>
+
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+
+    <script>
+    $(function(){
+
+    ___FUNCTIONAL_BIT___
+
+    });
+    </script>
+
+    <!-- DATASETS -->
+
+    ___DATASETS___
+
+    <!-- PIVOT TABLES -->
+
+    ___PIVOT_TABLES___
+
+    </body>
+    </html>
+    """
+
+    function dataset_to_html(data_label::Symbol, df::DataFrame)
+        io_buffer = IOBuffer()
+        CSV.write(io_buffer, df)
+        csv_string = String(take!(io_buffer))
+        html_str = replace(DATASET_TEMPLATE, "___DATA1___" => csv_string)
+        html_str = replace(html_str, "___DDATA_LABEL___" => replace(string(data_label), " " => "_"))
+        return html_str
+    end
+    function table_to_html(pt::PivotTable)
+        html_str = replace(PIVOTTABLE_IN_PAGE_TEMPLATE, "___TABLE_HEADING___" => string(pt.chart_title))
+        html_str = replace(html_str, "__FUNCTION_NAME___" => replace(string(pt.chart_title), " " => "_"))
+        return html_str
+    end
+
+
+
+    function create_pivot_table_html(pt::PivotTablePage, outfile_path::String="pivottable.html")
+        data_set_bit = reduce(*, [dataset_to_html(k, v) for (k,v) in pt.dataframes])
+        table_bit = reduce(*, [table_to_html(pti) for pti in pt.pivot_tables])
+        functional_bit = reduce(*, [pti.pivottable_html for pti in pt.pivot_tables])
+        full_page_html = replace(FULL_PAGE_TEMPLATE, "___DATASETS___" => data_set_bit)
+        full_page_html = replace(full_page_html, "___PIVOT_TABLES___" => table_bit)
+        full_page_html = replace(full_page_html, "___FUNCTIONAL_BIT___" => functional_bit)
+
+        open(outfile_path, "w") do outfile
+            write(outfile, full_page_html)
+        end
+
+        println("Pivot table page saved to $outfile_path")
+    end
+
+    export PivotTable, PivotTablePage, create_pivot_table_html
+
+end # module PivotTables
