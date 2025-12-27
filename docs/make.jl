ed1fbeea1b0e7707f5197f30434d8e3ed1b1006b
Stuart Baumann <13663771+s-baumann@users.noreply.github.com>
2025-12-28 10:23:01 +1100

    Documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,40 @@
-# Copy generated HTML examples to docs/src so Documenter includes them in build.
-# That is all we need to do as these example htmls are the documentation
+using Documenter, JSPlots
+
+# Build minimal Documenter site (just for deployment infrastructure)
+makedocs(;
+    modules=[JSPlots],
+    authors="Stuart Baumann",
+    repo="https://github.com/s-baumann/JSPlots.jl/blob/{commit}{path}#{line}",
+    sitename="JSPlots.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://s-baumann.github.io/JSPlots.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+    checkdocs = :none,
+    warnonly = true,
+)
+
+# Copy generated HTML examples to docs/build after makedocs
 examples_src = joinpath(@__DIR__, "..", "generated_html_examples")
-examples_dest = joinpath(@__DIR__, "src", "examples_html")
+build_dir = joinpath(@__DIR__, "build")
 if isdir(examples_src)
-    cp(examples_src, examples_dest, force=true)
-    println("Copied HTML examples to docs/src/examples_html/")
+    # Copy each subdirectory/file from generated_html_examples to build
+    for item in readdir(examples_src)
+        src_path = joinpath(examples_src, item)
+        dest_path = joinpath(build_dir, item)
+        cp(src_path, dest_path, force=true)
+    end
+    println("Copied HTML examples to docs/build/")
 else
     @warn "Generated HTML examples directory not found at: $examples_src"
 end
+
+# Deploy documentation
+deploydocs(;
+    repo="github.com/s-baumann/JSPlots.jl",
+    devbranch="main",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,5 @@
+# JSPlots.jl Documentation
+
+This documentation consists of live, interactive HTML examples demonstrating all JSPlots.jl features.
+
+Navigate to `z_general_example/z_general_example.html` to view the comprehensive tutorial with all chart types and capabilities.
