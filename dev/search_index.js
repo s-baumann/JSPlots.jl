2e60687dc6356d1ba5b1f44370e229585276c84c
Documenter.jl <documenter@juliadocs.github.io>
2025-12-28 01:13:04 +0000

    build based on 620ce13

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#JSPlots.jl-Documentation","page":"Home","title":"JSPlots.jl Documentation","text":"This documentation consists of live, interactive HTML examples demonstrating all JSPlots.jl features.\n\nNavigate to z_general_example/z_general_example.html to view the comprehensive tutorial with all chart types and capabilities.","category":"section"}]
+[{"location":"#JSPlots.jl-Documentation","page":"Home","title":"JSPlots.jl Documentation","text":"This documentation consists of live, interactive HTML examples demonstrating all JSPlots.jl features.\n\nNavigate to exampleshtml/zgeneralexample/zgeneral_example.html to view the comprehensive tutorial with all chart types and capabilities.","category":"section"}]
 }
